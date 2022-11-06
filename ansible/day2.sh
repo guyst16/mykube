ea7a21367aaf8c8e5e63eb2ef391065f77d2de7f
Guy Steinberger <guyst16@gmail.com>
2022-11-07 00:34:16 +0200

    redesign to the code but need a lookup

---
+++ ansible/day2.sh
@@ -0,0 +1,64 @@
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+dnf install -y containerd.io
+
+echo '[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]' >> /etc/containerd/config.toml
+echo '  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]' >> /etc/containerd/config.toml
+echo '    SystemdCgroup = true' >> /etc/containerd/config.toml
+
+sed -i '/disabled_plugins/d' /etc/containerd/config.toml 
+    
+systemctl restart containerd
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+setenforce 0
+
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe -a overlay br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
+swapoff -a;
+sed -i '/swap/d' /etc/fstab
+
+systemctl restart dbus;
+systemctl restart firewalld;
+
+firewall-cmd --permanent --add-port=6443/tcp;
+firewall-cmd --permanent --add-port=2379-2380/tcp;
+firewall-cmd --permanent --add-port=10250/tcp;
+firewall-cmd --permanent --add-port=10251/tcp;
+firewall-cmd --permanent --add-port=10252/tcp;
+firewall-cmd --permanent --add-port=10255/tcp;
+firewall-cmd --permanent --add-port=8472/udp;
+firewall-cmd --add-masquerade --permanent;
+firewall-cmd --permanent --add-port=30000-32767/tcp;
+systemctl status firewalld;
+systemctl restart firewalld;
+
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+kubeadm init
+
+systemctl enable --now kubelet
