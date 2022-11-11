c438cff1056cf2b23657d6ae8bd06d54f2fec89d
Guy Steinberger <guyst16@gmail.com>
2022-11-11 14:44:25 +0200

    Added to the start.sh script more steps

--- ansible/start.sh
+++ ansible/start.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-echo "Try deleting 'myFedoraVM' if exists..."
+echo -e "\nDownloading packages..."
+yum install qemu-kvm qemu-img libvirt libvirt-python libvirt-client virt-instal virt-viewer bridge-utils tigervnc-server
+
+echo -e "\nStart sshd service..."
+systemctl start sshd
+
+echo -e "\nStart libvirtd service..."
+systemctl start libvirtd
+
+echo -e "\nValidate that fedora image is ready..."
+if [ -f /var/lib/libvirt/images/Fedora-*.iso ]; then 
+	echo -e "\nFedora iso file exists"
+else 
+	echo -e "\nFile not exists\nStart download Fedora iso file..."
+	wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-dvd-x86_64-36-1.5.iso -P /var/lib/libvirt/images
+	echo -e "\nIso file is ready for use"
+fi
+
+
+echo -e "\nTry deleting 'myFedoraVM' if exists..."
 virsh destroy myFedoraVM; virsh undefine --remove-all-storage myFedoraVM
 
-echo "\n Start deploying the new vm..."
+echo -e "\nStart deploying the new vm..."
 virt-install -n myFedoraVM --description "my test Fedora vm" --os-variant=fedora36 --ram=2048 --vcpus=2 --disk path=/var/lib/libvirt/images/myFedoraVM.img,bus=virtio,size=20 --graphics none --location /root/Fedora-Server-dvd-x86_64-36-1.5.iso --initrd-inject /root/k8s-installer/ks.cfg --extra-args='inst.ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8' --noautoconsole --wait=-1
 
 # Waiting for IP address
-echo "\n Wait 20 seconds for IP address to get assigned..."
+echo -e "\nWait 20 seconds for IP address to get assigned..."
 sleep 20
 
 # Delete ssh fingerprint if exists
-echo "\n Delete fingerprint from ~/.ssh/known_hosts if exists..."
+echo -e "\nDelete fingerprint from ~/.ssh/known_hosts if exists..."
 ssh-keygen -f ~/.ssh/known_hosts -R $(virsh domifaddr --domain myFedoraVM | grep ':' | awk '{print $4}' | cut -d'/' -f1)
 
-echo "\n Run ansible-playbook for deploying k8s..."
+echo -e "\nRun ansible-playbook for deploying k8s..."
 ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook install-k8.yaml -e "ansible_password=qwe123" -i $(virsh domifaddr --domain myFedoraVM | grep ':' | awk '{print $4}' | cut -d'/' -f1), -b
